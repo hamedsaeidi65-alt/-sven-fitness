20abf249e16589c1ca803f57b54df54d1d298c50
hamedsaeidi65-alt <hamedsaeidi65@gmail.com>
2026-09-01 14:30:30 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sven-v4.15-cloud-offline-video";
+const CACHE = "sven-v4.16.1-google-drive-no-svg";
 const ASSETS = ["./manifest.json"];
 
 self.addEventListener("install", (event) => {
